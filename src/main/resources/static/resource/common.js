9bab0aa5866c45aa6a925b42a40a5205daeeab45
AnChangHyune <ymy6534@gmail.com>
2021-05-25 23:03:03 +0900

    검색바 디자인

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -5,3 +5,13 @@ function isTouchDevice() {
 }
 
 
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
